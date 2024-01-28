f2bba576d41262c513b43e84d454c1d73eff2fd6
Sujal-Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-01-28 15:02:14 +0530

    front page updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.weatherapi.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.weatherapi.com",
+        port: "",
+      },
+    ],
   },
 };
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,20 @@ export default async function Home() {
   return (
     <main className={styles.page}>
       <div className={styles.container}>
-        <h1 className={styles.head}>Welcome to Weather Forecast</h1>
+        <video
+          className={styles.video}
+          width="640"
+          height="360"
+          autoPlay
+          loop
+          muted
+        >
+          <source src="/earth.mp4" type="video/mp4" />
+        </video>
+
+        <h1 className={styles.head}>Hey there!</h1>
         <p className={styles.desc}>
-          Find the weather for your current city with a single search.
+          Curious about the weather in your city? Find out with a quick search!
         </p>
       </div>
     </main>
